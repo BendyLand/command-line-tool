8b67eaf57eca30d231ff1a1bb24212f3c089185f
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-02-26 16:05:34 -0500

    Start brainstorming ruby implementation of cli

---
+++ ruby/script.rb
@@ -0,0 +1,16 @@
+=begin
+Ruby project:
+Dictionary lookup from: 
+    https://dictionaryapi.dev/
+
+Usage: 
+The basic syntax of a URL request to the API is shown below:
+
+https://api.dictionaryapi.dev/api/v2/entries/en/<word>
+
+As an example, to get definition of English word hello, you can send request to
+
+https://api.dictionaryapi.dev/api/v2/entries/en/hello
+=end
+
+
