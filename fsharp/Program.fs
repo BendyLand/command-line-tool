04ef309b5e83361228a2e0795ad792a3bf1a4ff4
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-03-05 13:19:37 -0500

    Create basic structure for user interaction

--- fsharp/Program.fs
+++ fsharp/Program.fs
@@ -4,10 +4,21 @@
 ? The "cli" part will be selecting the amount and type of data to include in the resulting file (see notes.txt)
 _*)
 
+open System
+
 printfn """
 Welcome to the Random Log Generator CLI!
 Please select which type of data you're looking for: 
 1.) Some jumbled data
-2.) A lot of organized data
+2.) A lot of organized data 
 """
 
+let input = Console.ReadLine()
+let generateData input = 
+    match input with
+    | "1" -> printfn $"Generating some jumbled data..."
+    | "2" -> printfn $"Generating a lot of organized data..."
+    | _ -> printfn $"Invalid input"
+
+
+generateData input
---
+++ fsharp/README.md
@@ -0,0 +1,8 @@
+# F# Random Log File Generator
+
+This program randomly generates a certain number of sample logs and writes them to a file. 
+
+## Purpose
+
+I got the idea for this project as I was working on a different one; a log file parser. I realized that rather than searching for sample log data, I could dedicate another one of these projects to generating the file itself. 
+
