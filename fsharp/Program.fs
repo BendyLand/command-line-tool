62052c65303d1bafe80a44ba607dd46da3d6c443
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-03-05 13:03:21 -0500

    Refine notes and actually begin project

--- fsharp/Program.fs
+++ fsharp/Program.fs
@@ -1,2 +1,13 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿(* 
+? Idea:
+? Create a random Log generator to be used by a different program for data analysis.
+? The "cli" part will be selecting the amount and type of data to include in the resulting file (see notes.txt)
+_*)
+
+printfn """
+Welcome to the Random Log Generator CLI!
+Please select which type of data you're looking for: 
+1.) Some jumbled data
+2.) A lot of organized data
+"""
+
