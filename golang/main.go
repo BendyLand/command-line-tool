fafd327b7e179203c87cd62525a3f2fae45801c6
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-03-04 22:30:43 -0500

    Add basic adding functionality

--- golang/main.go
+++ golang/main.go
@@ -1,21 +1,33 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 )
 
 func main() {
 	fmt.Println("Welcome to the Go Todo List!")
 	input := getInput()
 
+    var tasks []string
+
 	switch input {
-		case "add": 
-			fmt.Println("You typed add")
-		default: 
-			fmt.Println("Invalid command")
-			os.Exit(1)
+	case "add":
+        var newTask string
+		fmt.Println("Please enter the task you would like to add: ")
+        fmt.Scanln(&newTask)
+        addTask(&tasks, newTask)
+	default:
+		fmt.Println("Invalid command")
+		os.Exit(1)
 	}
+    for i, item := range tasks {
+        fmt.Printf("Item %d.) %s\n", i+1, item)
+    }
+}
+
+func addTask(tasks *[]string, task string) {
+    *tasks = append(*tasks, task)
 }
 
 func getInput() string {
@@ -24,5 +36,3 @@ func getInput() string {
 	fmt.Scan(&input)
 	return input
 }
-
-
