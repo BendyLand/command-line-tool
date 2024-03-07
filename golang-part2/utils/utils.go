0ed0379a4bf6e68f69208fd102a383a7249b0850
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-03-06 20:23:40 -0500

    Create functions to construct different formats of datetimes

--- golang-part2/utils/utils.go
+++ golang-part2/utils/utils.go
@@ -6,6 +6,29 @@ import (
 	"math/rand"
 )
 
+/*  
+2001-02-13T05:16:14 INFO [Security] Successful login: IP address 137.221.132.152.201, username 'admin'
+1974-01-01T05:34:59 INFO [Database] Executing SQL query: SELECT col1, col2 FROM table_name WHERE col2 IS NOT NULL
+1997-06-11T11:27:18 INFO [SystemMonitor] CPU utilization: 30%, Memory usage: 60%, Disk space available: 50%
+2010-08-26T02:54:02 ERROR [UserManagementService] Error processing user registration request: User email already exists
+247.102.65.159.61 - - [30/Sep/2022:09:38:29 +0000] "POST /index.html HTTP/1.1" 200 1301
+*/
+
+func CreateRequestDateTime(date, time string) string {
+	return "[" + date + ":" + time + " +0000]"
+}
+
+func CreateStandardDateTime(date, time string) string {
+	components := []string{date, time}
+	return strings.Join(components, "T")
+}
+
+func ChooseRandomMessageOrigin() string {
+	origins := []string{"[Security]", "[Database]", "[SystemMonitor]", "[UserManagementService]"}
+	num := randomNumUnder(4)
+	return origins[num]
+}
+
 func GenerateRandomIp() string {
 	var nums []string
 	for i := 0; i < 5; i++ {
