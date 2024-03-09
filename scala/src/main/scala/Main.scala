a311aa30d1aba9c12c797aab3f6e632827b7d6ae
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-03-08 19:38:03 -0500

    Add deleteNote method

--- scala/src/main/scala/Main.scala
+++ scala/src/main/scala/Main.scala
@@ -3,6 +3,10 @@ import bland.notebook.*
 @main def run() = 
     val nb = Notebook()
     nb.createNote("This is a test note")
+    nb.createNote("This is another test note")
+    nb.createNote("This is a third test note")
+    nb.displayNotes
+    nb.deleteNote(2)
     nb.displayNotes
 
 
--- scala/src/main/scala/Notebook.scala
+++ scala/src/main/scala/Notebook.scala
@@ -6,13 +6,26 @@ class Notebook:
     var notes = List.empty[Note]
     var currentId = 0
 
-    def createNote(text: String): Note = 
+    def createNote(text: String): Note =
         val note = Note(currentId, text)
         notes = note :: notes
         currentId += 1
         note
 
-    def displayNotes: Unit = 
+    def displayNotes: Unit =
         println("Here are your notes: ")
         for i <- 1 to notes.size do
             println(s"$i.) ${notes(i-1).body}")
+
+    def deleteNote(id: Int): Boolean =
+        var newList = List.empty[Note]
+        notes.foreach { note =>
+            if note.id != id-1 then
+                newList = note :: newList
+        }
+        var result = true
+        if newList.size == notes.size then
+            println("Warning, possible invalid note ID. Please double check your notebook.")
+            result = false
+        notes = newList
+        result
