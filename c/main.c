690c2219bbcd2e79843466ca00f98e14141c2339
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-03-10 11:09:17 -0400

    Add comments to explain createTask

--- c/main.c
+++ c/main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 #define MAX_CHARS 100
-#define MAX_TASKS 10
+#define MAX_TASKS 100
 
 typedef struct {
     int id;
@@ -22,13 +22,32 @@ void display() {
 }
 
 void createTask() {
+
+    // check if the task list is full
     if (numTasks < MAX_TASKS) {
+
+        // increments the currentTask, gets the pointer to the `tasks` list, and assigns to newTask
         Task* newTask = &tasks[numTasks++];
+
+        // sets newTask's id to the current task number
         newTask->id = numTasks; 
+
+        // prompts the user for text to enter into the task body
         printf("Enter the task body for task %d: ", newTask->id);
+
+        // create a temporary storage location for the text
         char buffer[100]; 
+
+        // get user input of sizeof(buffer), 
         fgets(buffer, sizeof(buffer), stdin); 
+
+        // finds the index of '\n' with strcspn, 
+        // uses it as the index to buffer to find the end of the input, 
+        // and replace the newline with a NULL character
         buffer[strcspn(buffer, "\n")] = '\0'; 
+
+        // sets the body of newTask to the input, copied from buffer.
+        // strdup allocates memory as if malloc was called, so it needs to be free()d
         newTask->body = strdup(buffer);
     } else {
         printf("Task list if full!\n");
