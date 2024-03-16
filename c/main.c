5339fd84013b7d21a90be8b4b709b56637c95a34
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-03-16 15:09:04 -0400

    Create cli loop in c

--- c/main.c
+++ c/main.c
@@ -13,6 +13,7 @@ typedef struct {
 Task tasks[MAX_TASKS];
 int numTasks = 0;
 
+void run();
 void display();
 void cleanup();
 void createTask();
@@ -20,19 +21,39 @@ void deleteTask();
 
 int main(void) {
     printf("Welcome to the C Task Manager!\n");
-
-    createTask();
-    createTask();
-    display();
-    deleteTask();
-    display();
-    cleanup();
+    run();
 
     return 0;
 }
 
+void run() {
+    printf("\nWhat would you like to do?\n1.) Create task\n2.) View tasks\n3.) Delete task\n4.) Exit\n\n");
+    char buffer[MAX_TASKS];
+    fgets(buffer, sizeof(buffer), stdin);
+    buffer[strcspn(buffer, "\n")] = '\0';
+    int id = atoi(&buffer[0]);
+    switch (id) {
+        case 1:
+            createTask();
+            run();
+            break;
+        case 2:
+            display();
+            run();
+            break;
+        case 3:
+            deleteTask();
+            run();
+            break;
+        default:
+            printf("Shutting down todo list...\n");
+            cleanup();
+            break;
+    }
+}
+
 void deleteTask() {
-    printf("Which Task ID would you like to delete?\n");
+    printf("Which Task (ID) would you like to delete?\n\n");
     display();
     // temporary storage for user input
     char buffer[MAX_TASKS];
@@ -47,7 +68,7 @@ void deleteTask() {
         // if the task id matches the user input...
         if (tasks[i].id == id) {
             // free the memory associated with current pointer
-            free(tasks[i].body); 
+            free(tasks[i].body);
             // set the value of the memory address to NULL to prevent double free in cleanup()
             tasks[i].body = NULL;
             // print success message and return
@@ -74,7 +95,7 @@ void createTask() {
     // check if the task list is full
     if (numTasks < MAX_TASKS) {
         // increments the currentTask, gets the pointer to the `tasks` list at that index, and assigns to newTask
-        Task* newTask = &tasks[numTasks++]; 
+        Task* newTask = &tasks[numTasks++];
         // sets newTask's id to the current task number
         newTask->id = numTasks;
         // prompts the user for text to enter into the task body
@@ -91,11 +112,12 @@ void createTask() {
         // strdup allocates memory as if malloc was called, so it needs to be free()d
         newTask->body = strdup(buffer);
     } else {
-        printf("Task list if full!\n");
+        printf("Task list is full!\n");
     }
 }
 
 void display() {
+    printf("Here are your tasks:\n");
     for (int i = 0; i < MAX_TASKS; i++) {
         if (tasks[i].body != NULL) {
             printf("Task ID %d: %s\n", tasks[i].id, tasks[i].body);
